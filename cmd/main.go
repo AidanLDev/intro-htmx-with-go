56e5be352e867d929dc8e2f6405d3c81af7382eb
AidanLDev <dev@aidanlowson.com>
2024-04-13 12:32:42 +0100

    Adding errors

--- cmd/main.go
+++ cmd/main.go
@@ -62,24 +62,67 @@ func newData() Data {
     }
 }
 
+func (d Data) hasEmail(email string) bool {
+    for _, contact := range d.Contacts {
+        if (contact.Email == email) {
+            return true
+        }
+    }
+    return false
+}
+
+type FormData struct {
+    Values map[string]string
+    Errors map[string]string
+} 
+
+func newFormData() FormData {
+    return FormData{
+        Values: make(map[string]string),
+        Errors: make(map[string]string),
+    }
+}
+
+type Page struct {
+    Data Data
+    Form FormData
+}
+
+func newPage() Page {
+    return Page {
+        Data: newData(),
+        Form: newFormData(),
+    }
+}
+
 func main() {
 	e := echo.New()
     e.Renderer = NewTemplates()
     e.Use(middleware.Logger())
 
-    data := newData()
+    page := newPage()
 
     e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index", data)
+        return c.Render(200, "index", page)
     })
 
     e.POST("/contacts", func(c echo.Context) error {
         name := c.FormValue("name")
         email := c.FormValue("email")
 
-        data.Contacts = append(data.Contacts, newContact(name, email))
+        if (page.Data.hasEmail(email)) {
+            formData := newFormData()
+            formData.Values["name"] = name
+            formData.Values["email"] = email
+
+            formData.Errors["email"] = "Email already exists"
+            return c.Render(422, "form", formData)
+        }
+
+        page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
         
-        return c.Render(200, "index", data)
+        // TODO: ???
+        return c.Render(200, "display", page)
     })
 
     e.GET("/blocks", func(c echo.Context) error {
